ad94b9dcab0fd32cdb1e918e6a942073c5ae0da7
evachan246 <chingwa0812@gmail.com>
2022-04-01 12:29:47 +0800

    compress png

--- change_size.py
+++ change_size.py
@@ -4,8 +4,8 @@ from apng import APNG
 import time
 import glob
 import os
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import cv2
+import numpy as np
 #from scripts.apng_square_and_optimize import *
 
 newsize = (512, 512)
@@ -34,12 +34,26 @@ def resize_with_padding(img, expected_size):
     padding = (pad_width, pad_height, delta_width - pad_width, delta_height - pad_height)
     return ImageOps.expand(img, padding, fill=(0,0,0,0))
 
-def resize_png(img, expected_size):
-    height = expected_size[0]
-    width = expected_size[1]
-    color = (0, 0, 0)
-    cv2.imread(img)
-    old_image_height, old_image_width, channels = img.shape
+def resize_png(img_path, expected_size, png_file_size):
+    img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    height, width, channels = img.shape
+    new_height, new_width = expected_size
+    delta_width = new_width - width
+    delta_height = new_height - height
+    pad_width = delta_width // 2
+    pad_height = delta_height // 2
+
+    img_with_border = cv2.copyMakeBorder(img, pad_height, pad_height, pad_width, pad_width, cv2.BORDER_CONSTANT, value=[0, 0, 0, 0])
+    # img_with_border = img
+    cv2.imwrite(img_path, img_with_border, [int(cv2.IMWRITE_PNG_COMPRESSION), 80])
+
+    img_file_size = os.path.getsize(img_path)
+    if(png_file_size < img_file_size):
+        img = Image.open(img_path)
+        assert img.mode == 'RGBA'
+        out_pil = img.convert(mode="P", palette=Image.ADAPTIVE)
+        out_pil.save(img_path, "PNG", quality=95, optimize=True)
+        #img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
 
 
 def resize_apng(path, img, fn):
@@ -47,12 +61,14 @@ def resize_apng(path, img, fn):
     im_list = []
     path = path+"\\"+str(fn).split('\\')[-1].split('.')[0]
     os.makedirs(path, exist_ok=True)
+    frame_len = len(img.frames)
+    png_file_size = 300 * 1024 / frame_len
     for png, control in img.frames:
         filename = path+"\{i}.png".format(i=i)
         png.save(filename)
-       # img = resize_png(filename, [512,512])
-        img = resize_with_padding(Image.open(filename), [512, 512])
-        img = img.save(filename, 'PNG')
+        img = resize_png(filename, [512,512], int(png_file_size))
+       # img = resize_with_padding(Image.open(filename), [512, 512])
+        #img = img.save(filename, 'PNG')
         im_list.append(img)
         i+=1
     apngImg = APNG()
@@ -74,7 +90,7 @@ def create_animated_sticker(rootdir):
             im_list = []
             for png, control in im.frames:
                 png.save("image/{i}.png".format(i=i))
-                image = Image.open("image/{i}.png".format(i=i))
+                image = Image.open(open("image/{i}.png".format(i=i), 'rb'))
                 image = resize_with_padding(image, [512,512])
                 image.save("image/{i}-{i}.png".format(i=i),optimize=True,quality=30)
                 os.remove("image/{i}.png".format(i=i))
